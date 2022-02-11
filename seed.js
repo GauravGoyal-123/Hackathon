63555c37fe2443d1fc11626bd977d07a5eb1fd94
Chakshan dhir <89823586+chakshandhir004@users.noreply.github.com>
2022-02-11 12:43:16 +0530

    Update seed.js

--- seed.js
+++ seed.js
@@ -13,7 +13,7 @@ const prblm = [
     },
     {
         topic:"Linked-List",
-        level:"Medium",
+        level:"Hard",
         desc:"You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.You may assume the two numbers do not contain any leading zero, except the number 0 itself."
         
     },
